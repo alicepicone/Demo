2b0052830adb4a42177c55b5f4ad6fd7a0fadc6e
Alizetta <alice.picone17@gmail.com>
2024-09-03 09:46:36 +0200

    aggiunge dipendenza lombok e aggiunge annotation in person

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -3,28 +3,22 @@ package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private final int id;
+    private int id;
+
     @NotBlank
-    private final String name;
+    private String name;
 
-    public Person(@JsonProperty("id") int id,
-                  @JsonProperty("name") String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
